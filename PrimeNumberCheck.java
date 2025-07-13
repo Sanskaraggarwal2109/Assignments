7220315e023e82b6cc81b98647c49e94898ee671
Sanskaraggarwal2109 <144722094+Sanskaraggarwal2109@users.noreply.github.com>
2025-07-13 22:01:37 +0530

    Update PrimeNumberCheck.java

--- PrimeNumberCheck.java
+++ PrimeNumberCheck.java
@@ -3,20 +3,35 @@ package assignments;
 import java.util.Scanner;
 
 public class PrimeNumberCheck {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      int num;
-      Scanner sc=new Scanner(System.in);
-      System.out.println("Enter a number:");
-      num=sc.nextInt();
-      
-      if (num%2 !=0&&num%num%3!=0) {
-    	  System.out.println("The number is prime");
-      }
-      else {
-    	  System.out.println("The number is not prime");
-      }
-	}
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num <= 1) {
+            System.out.println(num + " is not a prime number.");
+        } else {
+            int i = 2;
+            int count = 0;
+
+            while (i <= Math.sqrt(num)) {
+                if (num % i == 0) {
+                    count++;
+                    break;
+                }
+                i++;
+            }
+
+            if (count == 0)
+                System.out.println(num + " is a prime number.");
+            else
+                System.out.println(num + " is not a prime number.");
+        }
+
+        sc.close();
+    }
 }
+
+
